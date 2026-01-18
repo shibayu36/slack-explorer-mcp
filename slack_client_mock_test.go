dc9b742096eef207ce657b9fca474c3cd7cf72f6
shibayu36 <shibayu36@gmail.com>
2026-01-18 15:24:18 +0900

    Add SearchFiles method to SlackClient interface and implementation

--- slack_client_mock_test.go
+++ slack_client_mock_test.go
@@ -18,6 +18,15 @@ func (m *SlackClientMock) SearchMessages(query string, params slack.SearchParame
 	return res, args.Error(1)
 }
 
+func (m *SlackClientMock) SearchFiles(query string, params slack.SearchParameters) (*slack.SearchFiles, error) {
+	args := m.Called(query, params)
+	var res *slack.SearchFiles
+	if v := args.Get(0); v != nil {
+		res = v.(*slack.SearchFiles)
+	}
+	return res, args.Error(1)
+}
+
 func (m *SlackClientMock) GetConversationReplies(params *slack.GetConversationRepliesParameters) ([]slack.Message, bool, string, error) {
 	args := m.Called(params)
 	msgs, _ := args.Get(0).([]slack.Message)
